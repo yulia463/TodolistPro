698bea23a930d0ac954aaf9d449d3dac859379ee
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-01-31 11:14:04 +0100

    monday 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,23 @@ import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
-const tasks1 = [
-        { id: 1, title: "HTML&CSS", isDone: true },
-        { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false }
+
+    const tasks1 = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false}
     ]
     const tasks2 = [
-        { id: 1, title: "Hello world", isDone: true },
-        { id: 2, title: "I am Happy", isDone: false },
-        { id: 3, title: "Yo", isDone: false }
+        {id: 1, title: "Hello world", isDone: true},
+        {id: 2, title: "I am Happy", isDone: false},
+        {id: 3, title: "Yo", isDone: false}
     ]
     return (
         <div className="App">
             <Todolist title={"1"} tasks={tasks1}/>
-            <Todolist title={"2"}  tasks={tasks2}/>
+            <Todolist title={"2"} tasks={tasks2}/>
         </div>
     );
 }
+
 export default App;
