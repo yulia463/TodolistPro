d281506d36bbdae36ad23c7f554cbec232770aa2
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-01-31 11:12:42 +0100

    monday 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tasks1 = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false }
+    ]
+    const tasks2 = [
+        { id: 1, title: "Hello world", isDone: true },
+        { id: 2, title: "I am Happy", isDone: false },
+        { id: 3, title: "Yo", isDone: false }
+    ]
+    return (
+        <div className="App">
+            <Todolist title={"1"} tasks={tasks1}/>
+            <Todolist title={"2"}  tasks={tasks2}/>
+        </div>
+    );
 }
-
 export default App;
---
+++ src/Todolist.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+type TodolistPropsType = {
+    title: string
+    tasks: Array<TasksPropsType>
+}
+type TasksPropsType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+export const Todolist = (props: TodolistPropsType) => {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {props.tasks.map((el) => {
+                    return <li key={el.id}><input type="checkbox" checked={el.isDone}/><span>{el.title}</span></li>
+                })}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    )
+}
